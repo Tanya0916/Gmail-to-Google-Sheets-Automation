f5c8b4a5874fbeaf810aaf1d4a8fc62e3d3bcda0
Tanya <tanya09chauhan@gmail.com>
2026-01-14 19:05:19 +0530

    Refactor get_unread_emails function
    Refactor get_unread_emails function to streamline code and improve readability.

--- src/gmail_service.py
+++ src/gmail_service.py
@@ -1,7 +1,4 @@
-
-from __future__ import print_function
 import os.path
-import pickle
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -11,25 +8,22 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 def get_unread_emails():
     creds = None
-  
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-   
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0) 
-      
+            creds = flow.run_local_server(port=0)   
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
     results = service.users().messages().list(userId='me', labelIds=['UNREAD']).execute()
-    messages = results.get('messages', [])
-    return messages
+    return results.get('messages', [])
+
 
 
 
