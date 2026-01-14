cd8fa3836bd1bd2a813c84330494869a79a4e28b
Tanya <tanya09chauhan@gmail.com>
2026-01-14 19:00:11 +0530

    Refactor Gmail service to improve email fetching
    Refactored Gmail service to fetch unread emails using a new method and updated token handling.

--- src/gmail_service.py
+++ src/gmail_service.py
@@ -1,32 +1,35 @@
-from googleapiclient.discovery import build
+
+from __future__ import print_function
+import os.path
+import pickle
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-import os, pickle
+from googleapiclient.discovery import build
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def get_gmail_service():
+def get_unread_emails():
     creds = None
-    token_path = 'credentials/token.pickle'
-    creds_path = 'credentials/credentials.json'
-
-    if os.path.exists(token_path):
-        with open(token_path, 'rb') as token:
-            creds = pickle.load(token)
-
+  
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+   
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open(token_path, 'wb') as token:
-            pickle.dump(creds, token)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0) 
+      
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-    return build('gmail', 'v1', credentials=creds)
-
-def fetch_unread_emails(service):
-    results = service.users().messages().list(userId='me', labelIds=['INBOX'], q="is:unread").execute()
+    service = build('gmail', 'v1', credentials=creds)
+    results = service.users().messages().list(userId='me', labelIds=['UNREAD']).execute()
     messages = results.get('messages', [])
     return messages
+
+
+
