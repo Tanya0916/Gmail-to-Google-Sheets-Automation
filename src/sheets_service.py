ef3ec179e99e341fd0fe85c57b631fecb05053a0
Tanya <tanya09chauhan@gmail.com>
2026-01-14 11:31:55 +0530

    Implement sheets service for Google Sheets API

---
+++ src/sheets_service.py
@@ -0,0 +1,25 @@
+def get_sheets_service():
+    creds = None
+    token_path = 'credentials/sheets_token.pickle'
+    creds_path = 'credentials/credentials.json'
+
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+        creds = flow.run_local_server(port=0)
+        with open(token_path, 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('sheets', 'v4', credentials=creds)
+
+def append_row(service, row):
+    body = {'values': [row]}
+    service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption="RAW",
+        body=body
+    ).execute()
