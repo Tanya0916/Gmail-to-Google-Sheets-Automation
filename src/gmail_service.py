20108109e17f8c09534e4d692879e33a57424592
Tanya <tanya09chauhan@gmail.com>
2026-01-14 11:28:52 +0530

    Add Gmail service integration
    Implement Gmail service to fetch unread emails.

---
+++ src/gmail_service.py
@@ -0,0 +1,32 @@
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os, pickle
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+def get_gmail_service():
+    creds = None
+    token_path = 'credentials/token.pickle'
+    creds_path = 'credentials/credentials.json'
+
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_path, 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('gmail', 'v1', credentials=creds)
+
+def fetch_unread_emails(service):
+    results = service.users().messages().list(userId='me', labelIds=['INBOX'], q="is:unread").execute()
+    messages = results.get('messages', [])
+    return messages
